dab7d535f7f893222971e2fee12355850e781851
Sumeetrana <sumeetsinh28@gmail.com>
2020-09-15 01:43:27 +0530

    SwipeButtons Design implemented

--- src/components/SwipeButtons/SwipeButtons.js
+++ src/components/SwipeButtons/SwipeButtons.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './SwipeButtons.css';
-
+import IconButton from '@material-ui/core/IconButton';
+import ReplayIcon from '@material-ui/icons/Replay';
+import CloseIcon from '@material-ui/icons/Close';
+import StarRateIcon from '@material-ui/icons/StarRate';
+import FavouriteIcon from '@material-ui/icons/Favorite';
+import FlashIcon from '@material-ui/icons/FlashOn';
 
 const SwipeButtons = () => {
     return (
-        <div>
-            SwipeButtons            
+        <div className="swipeButtons">
+            <IconButton className="swipeButtons__repeat">
+                <ReplayIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__left">
+                <CloseIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__star">
+                <StarRateIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__right">
+                <FavouriteIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__lightning">
+                <FlashIcon fontSize="large" />
+            </IconButton>
         </div>
     );
 };
