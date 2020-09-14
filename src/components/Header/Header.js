e6041e3533b202561ffebe9cbaea999a7000bc8e
Sumeetrana <sumeetsinh28@gmail.com>
2020-09-15 00:48:22 +0530

    Header style  completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
 import './Header.css';
+
+import IconButton from '@material-ui/core/IconButton';
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
 
 const Header = () => {
     return (
-        <div className="Header">
-            Header
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon" />
+            </IconButton>
+
+            <img 
+                className="header__logo"
+                src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+                alt="Tinder Logo"
+            />
+
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon" />
+            </IconButton>
         </div>
     );
 };
